01a1ef3f5c32f7a3cf2c9519218fee2eaeda9867
Nicholas Armstrong-Crews <nickarmstrongcrews@gmail.com>
2022-02-05 12:23:55 -0800

    shell script doc

--- data/visible_wand_office/convert.sh
+++ data/visible_wand_office/convert.sh
@@ -1,2 +1,3 @@
+# record of commands I used to reduce file size before commit by cropping and converting to jpg
 files=$(ls visible_wand*.bmp) ; for f in $files ; do convvert $f -crop 652x366+26+172 $(basename $f .bmp)_cropped.bmp ; done
 for f in $(ls *.bmp) ; do convert -quality 95 $f $(basename $f .bmp).jpg ; done
